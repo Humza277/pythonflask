7cefc25a7d1d9758d40ad7fc6cf8eea92da13e70
hmalak <hmalak@spartaglobal.com>
2020-07-24 15:37:40 +0100

    files removed

--- tabledef.py
+++ tabledef.py
@@ -20,28 +20,3 @@ class User(Base):
         Base.metadata.create_all(engine)
 
 
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            attempt = int(session.get('attempt'))
-            if attempt == 2:
-                flash("This is your last chance")
-                # attempt += 1
-                # session['attempt'] = attempt
-            if attempt == 3:
-                flash('You have been logged out.')
-                abort(404)
-            else:
-                attempt += 1
-                session['attempt'] = attempt
-                error = 'Invalid Credentials. Please try again'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in.')
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
-
-def home():
-    session['attempt'] = 1
-    return render_template('index.html')  # render a template
